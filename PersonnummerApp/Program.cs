66af9c9537675b877f09db028ba04c32afb8935e
mohasan99 <mo70hasan@gmail.com>
2026-01-14 09:29:57 +0100

    Lagt till logik för personnummer

--- PersonnummerApp/Program.cs
+++ PersonnummerApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PersonnummerApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Skriv in personnummer: ");
+            string input = Console.ReadLine();
+
+            if (PersonnummerValidator.IsValid(input)) {
+                Console.WriteLine("Giltigt! ✅");
+            } else {
+                Console.WriteLine("Ogiltigt! ❌");
+            }
+        }
+    }
+}
